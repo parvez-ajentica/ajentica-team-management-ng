35eb6e9dd54b5d1d0d2ee064e0843976186396af
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-18 07:30:32 +0600

    9: Setup Angular Routing with Placeholder Pages (#62)
    * remove blankspace from app.config.ts
    
    * 9: add dashboard component
    
    - add dashboard component with placeholder content
    
    * 9: add teams component and remove .gitkeep file
    
    - remove .gitkeep file from dashboard folder
    - remove .gitkeep file from teams folder
    - remove .gitkeep file from features folder
    - add teams component with html, css, ts and spec files
    
    * 9: add member component
    
    - add member component with html, css, ts and spec files
    - removed .gitkeep file from members folder
    
    * 9: add projects component
    
    - add projects component with html, css, ts and spec files
    - remove .gitkeep file from projects folder
    
    * 9: add angular routing and router outlet
    
    - add angular routing
    - add router outlet to app component and app html
    
    * 9: Setup Angular routing with placeholder pages
    
    - link dashboard, teams, members, and projects in sidebar to their respective routes
    - update sidebar component to use routerLink for navigation

--- ajentica-team-management-frontend/src/app/app.routes.ts
+++ ajentica-team-management-frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/dashboard/dashboard';
+import { Teams } from './features/teams/teams/teams';
+import { Members } from './features/members/members/members';
+import { Projects } from './features/projects/projects/projects';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: 'teams',
+    component: Teams,
+  },
+  {
+    path: 'members',
+    component: Members,
+  },
+  {
+    path: 'projects',
+    component: Projects,
+  },
+];
--- ajentica-team-management-frontend/src/app/app.ts
+++ ajentica-team-management-frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './layout/header/header/header';
 import { Sidebar } from './layout/sidebar/sidebar/sidebar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Sidebar],
+  imports: [Header, Sidebar, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
