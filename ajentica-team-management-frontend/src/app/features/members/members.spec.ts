40ed1d8de80d54e2bcd552187f61e55a164558ec
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-18 22:53:20 +0600

    66: fix folder structure
    - remove duplicate directories from component
    - remove the redundant directories from components
    - update the imports in app.routes.ts and app.ts

--- ajentica-team-management-frontend/src/app/features/members/members.spec.ts
+++ ajentica-team-management-frontend/src/app/features/members/members.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Members } from './members';
+import { Members } from '../members';
 
 describe('Members', () => {
   let component: Members;
