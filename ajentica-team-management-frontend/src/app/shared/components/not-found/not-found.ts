354829625ca86357f490436f4a7b8c46e565f606
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-20 20:04:03 +0600

    16 update error 404 not found page layout
    - create a layout for error 404 not found page
    - add a button to navigate back to the previous page
    - add a message to inform the user that the page is not found
    - add stlyes to error 404 not found page

--- ajentica-team-management-frontend/src/app/shared/components/not-found/not-found.ts
+++ ajentica-team-management-frontend/src/app/shared/components/not-found/not-found.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css',
 })
-export class NotFound {}
+export class NotFound {
+  constructor(private location: Location) {}
+
+  goBack() {
+    this.location.back();
+  }
+}
