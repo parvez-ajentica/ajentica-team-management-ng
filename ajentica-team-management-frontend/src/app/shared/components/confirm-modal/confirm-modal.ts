01f96badc08e1e93c1e0d0117b48d3241d316c37
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-22 05:05:58 +0600

    28: Add Delete Confirmation UI (#79)
    * 28: create confirm modal component
    
    * 28: create modal component
    
    - create modal comnponent
    - add modal ui and functions
    - add modal to team page
    - add modal action to delete icon
    - add team page function to activate modal
    
    * 28: add temp dir to gitignore

---
+++ ajentica-team-management-frontend/src/app/shared/components/confirm-modal/confirm-modal.ts
@@ -0,0 +1,25 @@
+import { Component, input, output, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-modal',
+  imports: [],
+  templateUrl: './confirm-modal.html',
+  styleUrl: './confirm-modal.css',
+})
+export class ConfirmModal {
+  itemName = input<string>('');
+
+  isOpen = input<boolean>(false);
+
+  confirm = output<void>();
+  cancel = output<void>();
+
+  close() {
+    this.cancel.emit();
+  }
+
+  onConfirm() {
+    console.log('content deleted');
+    this.confirm.emit();
+  }
+}
