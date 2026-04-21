119273a2db69e2099cc5fb2a670a12e42d6e4ac9
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-21 09:04:07 +0600

    25: add add-button component to teams page
    - fix add-button component path
    - add add-button to teams page

--- ajentica-team-management-frontend/src/app/features/teams/teams.ts
+++ ajentica-team-management-frontend/src/app/features/teams/teams.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Card } from '../../shared/card/card';
-import { LucideCirclePlus, LucideSquarePen, LucideTrash } from '@lucide/angular';
+import { LucideSquarePen, LucideTrash } from '@lucide/angular';
+import { AddButton } from '../../shared/components/add-button/add-button';
 
 @Component({
   selector: 'app-teams',
-  imports: [Card, LucideSquarePen, LucideTrash, LucideCirclePlus],
+  imports: [Card, LucideSquarePen, LucideTrash, AddButton, AddButton],
   templateUrl: './teams.html',
   styleUrl: './teams.css',
 })
