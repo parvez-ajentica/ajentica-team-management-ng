2c42b47624162f8f06f23533cbec912eaea6ff37
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-21 07:49:40 +0600

    25: add edit icon to team card
    - add edit icon to team card from lucide icons
    - update button stylesn for the icons

--- ajentica-team-management-frontend/src/app/features/teams/teams.ts
+++ ajentica-team-management-frontend/src/app/features/teams/teams.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Card } from '../../shared/card/card';
+import { LucideSquarePen } from '@lucide/angular';
 
 @Component({
   selector: 'app-teams',
-  imports: [Card],
+  imports: [Card, LucideSquarePen],
   templateUrl: './teams.html',
   styleUrl: './teams.css',
 })
