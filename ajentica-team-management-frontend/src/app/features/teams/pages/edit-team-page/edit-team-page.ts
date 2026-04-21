a4742f731848f179a09251eef75fe40fb79578df
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-21 12:33:38 +0600

    26: add back button component
    - add back button component
    - add back buttoton to edit team page

--- ajentica-team-management-frontend/src/app/features/teams/pages/edit-team-page/edit-team-page.ts
+++ ajentica-team-management-frontend/src/app/features/teams/pages/edit-team-page/edit-team-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BackButton } from '../../../../shared/components/back-button/back-button';
 
 @Component({
   selector: 'app-edit-team-page',
-  imports: [],
+  imports: [BackButton],
   templateUrl: './edit-team-page.html',
   styleUrl: './edit-team-page.css',
 })
---
+++ ajentica-team-management-frontend/src/app/shared/components/back-button/back-button.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { LucideMoveLeft } from '@lucide/angular';
+
+@Component({
+  selector: 'app-back-button',
+  imports: [LucideMoveLeft],
+  templateUrl: './back-button.html',
+  styleUrl: './back-button.css',
+})
+export class BackButton {}
