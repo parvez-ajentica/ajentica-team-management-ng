1ad7855bbb4181c92b152a7309abadb9e921a4eb
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-22 18:00:52 +0600

    33: add pages for member features
    - add member main page component
    - add edit page component
    - add add page component

---
+++ ajentica-team-management-frontend/src/app/features/members/pages/add-member-page/add-member-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-member-page',
+  imports: [],
+  templateUrl: './add-member-page.html',
+  styleUrl: './add-member-page.css',
+})
+export class AddMemberPage {}
---
+++ ajentica-team-management-frontend/src/app/features/members/pages/edit-member-page/edit-member-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-member-page',
+  imports: [],
+  templateUrl: './edit-member-page.html',
+  styleUrl: './edit-member-page.css',
+})
+export class EditMemberPage {}
