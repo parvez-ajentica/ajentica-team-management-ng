8f137997649ecece24aa5fe69a25b497f8e20281
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-22 17:38:28 +0600

    32: add action button (#83)
    - add edit icon button
    - add delete icon button
    - add view details button

--- ajentica-team-management-frontend/src/app/features/members/members.ts
+++ ajentica-team-management-frontend/src/app/features/members/members.ts
@@ -3,10 +3,21 @@ import { Card } from '../../shared/card/card';
 import { ProfileInfo } from '../../shared/components/profile-info/profile-info';
 import { ViewDetailsButton } from '../../shared/components/view-details-button/view-details-button';
 import { MemberService } from '../../core/services/member.service';
+import { RouterLink } from '@angular/router';
+import { LucideSquarePen, LucideTrash } from '@lucide/angular';
+import { ConfirmModal } from '../../shared/components/confirm-modal/confirm-modal';
 
 @Component({
   selector: 'app-members',
-  imports: [Card, ProfileInfo, ViewDetailsButton],
+  imports: [
+    Card,
+    ProfileInfo,
+    ViewDetailsButton,
+    RouterLink,
+    LucideSquarePen,
+    LucideTrash,
+    ConfirmModal,
+  ],
   templateUrl: './members.html',
   styleUrl: './members.css',
 })
@@ -16,4 +27,21 @@ export class Members {
   private memberService = inject(MemberService);
 
   members = this.memberService.getAllMembers();
+
+  isModalOpen = signal(false);
+  selectedName = signal('');
+
+  openModal(name: string) {
+    this.selectedName.set(name);
+    this.isModalOpen.set(true);
+  }
+
+  closeModal() {
+    this.isModalOpen.set(false);
+  }
+
+  handleConfirm() {
+    console.log('Confirmed delete for:', this.selectedName());
+    this.closeModal();
+  }
 }
