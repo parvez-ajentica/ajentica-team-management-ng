44d12017d59818fff5a1532a9a84cf48a9244a40
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-22 16:00:46 +0600

    30: create memnber card
    - create the basic member card
    - add basic styles

--- ajentica-team-management-frontend/src/app/features/members/members.ts
+++ ajentica-team-management-frontend/src/app/features/members/members.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Card } from '../../shared/card/card';
+import { ProfileInfo } from '../../shared/components/profile-info/profile-info';
+import { ViewDetailsButton } from '../../shared/components/view-details-button/view-details-button';
 
 @Component({
   selector: 'app-members',
-  imports: [Card],
+  imports: [Card, ProfileInfo, ViewDetailsButton],
   templateUrl: './members.html',
   styleUrl: './members.css',
 })
 export class Members {
   pageName: string = 'Members';
+
+  members = signal([
+    { id: 1, name: 'Alice', role: 'Backend Dev' },
+    { id: 2, name: 'Bob', role: 'UI Designer' },
+  ]);
 }
