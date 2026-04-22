c6289ae85d56de4ead5940381c815632560fc379
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-22 16:14:16 +0600

    30: update the member sevice
    - update the the member service
    - add member service to the member component

--- ajentica-team-management-frontend/src/app/core/services/member.service.ts
+++ ajentica-team-management-frontend/src/app/core/services/member.service.ts
@@ -1,6 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class MemberService {}
+export class MemberService {
+  private members = signal([
+    {
+      id: 1,
+      name: 'John Doe',
+      role: 'Senior Frontend Engineer',
+      teams: ['Frontend Team'],
+      projects: ['Rethink BH'],
+    },
+    {
+      id: 2,
+      name: 'Alice Smith',
+      role: 'Backend Developer',
+      teams: ['API Team'],
+      projects: ['Payment Gateway'],
+    },
+    {
+      id: 3,
+      name: 'Bob Johnson',
+      role: 'UI Designer',
+      teams: ['Design Team'],
+      projects: ['Website Redesign'],
+    },
+  ]);
+
+  getAllMembers() {
+    return this.members; // ✅ return signal
+  }
+}
--- ajentica-team-management-frontend/src/app/features/members/members.ts
+++ ajentica-team-management-frontend/src/app/features/members/members.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Card } from '../../shared/card/card';
 import { ProfileInfo } from '../../shared/components/profile-info/profile-info';
 import { ViewDetailsButton } from '../../shared/components/view-details-button/view-details-button';
+import { MemberService } from '../../core/services/member.service';
 
 @Component({
   selector: 'app-members',
@@ -12,8 +13,7 @@ import { ViewDetailsButton } from '../../shared/components/view-details-button/v
 export class Members {
   pageName: string = 'Members';
 
-  members = signal([
-    { id: 1, name: 'Alice', role: 'Backend Dev' },
-    { id: 2, name: 'Bob', role: 'UI Designer' },
-  ]);
+  private memberService = inject(MemberService);
+
+  members = this.memberService.getAllMembers();
 }
