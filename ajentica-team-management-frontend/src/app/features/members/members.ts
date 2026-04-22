8a377345ff37e94b311fcb907be69bf5aef71c01
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-22 16:39:32 +0600

    30: Create Reusable Member Card Component & 31: Display Members Using Placeholder (#82)
    * 30: create member card
    
    - create the basic member card
    - add basic styles
    
    * 30: add member service
    
    - add member service
    
    * 30: update the member sevice
    
    - update the the member service
    - add member service to the member component
    
    * 30: update the member page
    
    - update the member page with dynamic data
    - add funtions to apply the service

--- ajentica-team-management-frontend/src/app/features/members/members.ts
+++ ajentica-team-management-frontend/src/app/features/members/members.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Card } from '../../shared/card/card';
+import { ProfileInfo } from '../../shared/components/profile-info/profile-info';
+import { ViewDetailsButton } from '../../shared/components/view-details-button/view-details-button';
+import { MemberService } from '../../core/services/member.service';
 
 @Component({
   selector: 'app-members',
-  imports: [Card],
+  imports: [Card, ProfileInfo, ViewDetailsButton],
   templateUrl: './members.html',
   styleUrl: './members.css',
 })
 export class Members {
   pageName: string = 'Members';
+
+  private memberService = inject(MemberService);
+
+  members = this.memberService.getAllMembers();
 }
