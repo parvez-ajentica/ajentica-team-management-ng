be960605f60cbf44f953fbe8f9b9bb85a54dd564
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-23 09:57:06 +0600

    33: add view details page ui
    - update the view details page
    - add header and section
    - update the header with static data
    - update the section with info and activity card
    - add the static data to the cards

--- ajentica-team-management-frontend/src/app/features/members/pages/view-member-details-page/view-member-details-page.ts
+++ ajentica-team-management-frontend/src/app/features/members/pages/view-member-details-page/view-member-details-page.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { BackButton } from '../../../../shared/components/back-button/back-button';
+import { ProfileInfo } from '../../../../shared/components/profile-info/profile-info';
+import { EditButton } from '../../../../shared/components/edit-button/edit-button';
+import { Card } from '../../../../shared/card/card';
 
 @Component({
   selector: 'app-view-member-details-page',
-  imports: [],
+  imports: [BackButton, EditButton, Card],
   templateUrl: './view-member-details-page.html',
   styleUrl: './view-member-details-page.css',
 })
