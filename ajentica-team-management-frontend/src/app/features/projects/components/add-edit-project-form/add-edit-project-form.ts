3af19d27c24b313e286758ccde23263491e8e8d8
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-23 14:41:31 +0600

    37: create add edit project ui
    - update the project form component
    - connect the project form component with edit and add page
    - update styles

--- ajentica-team-management-frontend/src/app/features/projects/components/add-edit-project-form/add-edit-project-form.ts
+++ ajentica-team-management-frontend/src/app/features/projects/components/add-edit-project-form/add-edit-project-form.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed, input } from '@angular/core';
+import { MultiSelect } from '../../../../shared/components/multi-select/multi-select';
+import { JsonPipe } from '@angular/common';
+import { BackButton } from '../../../../shared/components/back-button/back-button';
 
 @Component({
   selector: 'app-add-edit-project-form',
-  imports: [],
+  imports: [MultiSelect, BackButton],
   templateUrl: './add-edit-project-form.html',
   styleUrl: './add-edit-project-form.css',
 })
-export class AddEditProjectForm {}
+export class AddEditProjectForm {
+  pageName = input<string>('');
+  projectName = signal('');
+  dueDate = signal(''); // yyyy-mm-dd
+  progress = signal<number | null>(null);
+
+  teams = signal<number[]>([]);
+
+  // ======================
+  // DATA
+  // ======================
+
+  teamList = signal([
+    { id: 1, name: 'Frontend Team' },
+    { id: 2, name: 'Backend Team' },
+    { id: 3, name: 'Design Team' },
+  ]);
+
+  // ======================
+  // DERIVED STATE
+  // ======================
+
+  formValue = computed(() => ({
+    projectName: this.projectName(),
+    dueDate: this.dueDate(),
+    progress: this.progress(),
+    teams: this.teams(),
+  }));
+
+  // ======================
+  // HANDLERS
+  // ======================
+
+  setValue(signalFn: any, value: string) {
+    signalFn.set(value);
+  }
+
+  setProgress(value: string) {
+    this.progress.set(value ? Number(value) : null);
+  }
+
+  submit() {
+    console.log('Project Form:', this.formValue());
+  }
+}
--- ajentica-team-management-frontend/src/app/features/projects/pages/add-project-page/add-project-page.ts
+++ ajentica-team-management-frontend/src/app/features/projects/pages/add-project-page/add-project-page.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { AddEditProjectForm } from '../../components/add-edit-project-form/add-edit-project-form';
 
 @Component({
   selector: 'app-add-project-page',
-  imports: [],
+  imports: [AddEditProjectForm],
   templateUrl: './add-project-page.html',
   styleUrl: './add-project-page.css',
 })
-export class AddProjectPage {}
+export class AddProjectPage {
+  pageName: string = 'Add New project';
+}
