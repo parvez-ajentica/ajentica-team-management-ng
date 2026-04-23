39c93b90e6bd0cc13c2b085e9e2da8eb10a5bbf8
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-23 15:26:18 +0600

    40: create view project details page
    - add the layout for project details page
    - add the basic data for the page

--- ajentica-team-management-frontend/src/app/features/projects/pages/view-project-details-page/view-project-details-page.ts
+++ ajentica-team-management-frontend/src/app/features/projects/pages/view-project-details-page/view-project-details-page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { BackButton } from '../../../../shared/components/back-button/back-button';
+import { EditButton } from '../../../../shared/components/edit-button/edit-button';
+import { Card } from '../../../../shared/card/card';
 
 @Component({
   selector: 'app-view-project-details-page',
-  imports: [BackButton],
+  imports: [BackButton, EditButton, Card],
   templateUrl: './view-project-details-page.html',
   styleUrl: './view-project-details-page.css',
 })
