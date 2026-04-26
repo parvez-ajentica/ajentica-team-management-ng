939ba995fce1ae29e1c17b2854ad85f2bb0d4e6b
Md Parvez Al Mohidul <parvez.mohidul@ajentica.ai>
2026-04-26 10:51:37 +0600

    44: update project api service
    - update project api to get , create, update and delete data
    - add environment url to to the project api service
    - update project service to match api
    - update project page

--- ajentica-team-management-frontend/src/app/core/services/api/projects.api.ts
+++ ajentica-team-management-frontend/src/app/core/services/api/projects.api.ts
@@ -1,6 +1,31 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environments';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProjectsApi {}
+export class ProjectsApi {
+  private http = inject(HttpClient);
+  private baseUrl = `${environment.apiUrl}/projects`;
+
+  getAll() {
+    return this.http.get<any[]>(this.baseUrl);
+  }
+
+  getById(id: number) {
+    return this.http.get<any>(`${this.baseUrl}/${id}`);
+  }
+
+  create(project: any) {
+    return this.http.post(this.baseUrl, project);
+  }
+
+  update(id: number, project: any) {
+    return this.http.put(`${this.baseUrl}/${id}`, project);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+}
